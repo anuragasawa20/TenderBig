5e4c5403a3ec5d3b290721ab9a2ad48970974102
Akshat103 <akshattamrakar103@gmail.com>
2023-06-26 22:47:12 +0530

    Fixed contact

--- client/src/Admin/partials/dashboard/ContactPage.jsx
+++ client/src/Admin/partials/dashboard/ContactPage.jsx
@@ -97,6 +97,7 @@ const ContactFormList = () => {
           <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+          <h1 className="text-xl font-bold mb-4">Contact Requests</h1>
             <table className="min-w-full divide-y divide-gray-200">
               <thead className="bg-gray-50">
                 <tr>
