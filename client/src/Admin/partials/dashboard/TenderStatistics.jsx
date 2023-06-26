f2f4e50f500d0aa161a91878a398c1dc47949c7f
Akshat103 <akshattamrakar103@gmail.com>
2023-06-26 18:51:22 +0530

    added charts at admin

--- client/src/Admin/partials/dashboard/TenderStatistics.jsx
+++ client/src/Admin/partials/dashboard/TenderStatistics.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Bar } from "react-chartjs-2";
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 const TenderStatistics = () => {
   const [statistics, setStatistics] = useState(null);
@@ -14,7 +17,10 @@ const TenderStatistics = () => {
           }
         };
 
-        const response = await axios.get("http://localhost:5000/apiTender/tenderdetails/statistics", config);
+        const response = await axios.get(
+          "http://localhost:5000/apiTender/tenderdetails/statistics",
+          config
+        );
         setStatistics(response.data);
       } catch (error) {
         console.error("Error fetching tender statistics:", error);
@@ -24,90 +30,115 @@ const TenderStatistics = () => {
     fetchTenderStatistics();
   }, []);
 
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-2xl font-bold mb-6">Tender Statistics</h1>
-      {statistics ? (
-        <div className="grid grid-cols-2 gap-4">
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4">Total Tenders</h2>
-            <p className="text-2xl font-bold">{statistics.totalCount}</p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4">Reviewed and Approved Tenders</h2>
-            <p className="text-2xl font-bold">{statistics.activeApprovedCount}</p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4">Reviewed Tenders</h2>
-            <p className="text-2xl font-bold">{statistics.activeCount}</p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4">Approved Tenders</h2>
-            <p className="text-2xl font-bold">{statistics.approvedCount}</p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4">Contractor Tenders</h2>
-            <p className="text-2xl font-bold">{statistics.contractorCount}</p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4">Subcontractor Tenders</h2>
-            <p className="text-2xl font-bold">{statistics.subcontractorCount}</p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4">Admin Tenders</h2>
-            <p className="text-2xl font-bold">{statistics.adminCount}</p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4">HR Tenders</h2>
-            <p className="text-2xl font-bold">{statistics.hrCount}</p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4">Employee Tenders</h2>
-            <p className="text-2xl font-bold">{statistics.employeeCount}</p>
-          </div>
-        </div>
-      ) : (
-        <div className="grid grid-cols-2 gap-4">
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4"></h2>
-            <p className="text-2xl font-bold"></p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4"></h2>
-            <p className="text-2xl font-bold"></p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4"></h2>
-            <p className="text-2xl font-bold"></p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4"></h2>
-            <p className="text-2xl font-bold"></p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4"></h2>
-            <p className="text-2xl font-bold"></p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4"></h2>
-            <p className="text-2xl font-bold"></p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4"></h2>
-            <p className="text-2xl font-bold"></p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4"></h2>
-            <p className="text-2xl font-bold"></p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4"></h2>
-            <p className="text-2xl font-bold"></p>
-          </div>
-        </div>
-      )}
-    </div>
+  const renderChart = () => {
+    if (!statistics) {
+      return null;
+    }
 
+    const chartData = {
+      labels: [
+        "Reviewed and Approved Tenders",
+        "Reviewed Tenders",
+        "Approved Tenders",
+        "Contractor Tenders",
+        "Subcontractor Tenders",
+        "Admin Tenders",
+        "HR Tenders",
+        "Employee Tenders"
+      ],
+      datasets: [
+        {
+          label: "Tender Statistics",
+          data: [
+            statistics.activeApprovedCount,
+            statistics.activeCount,
+            statistics.approvedCount,
+            statistics.contractorCount,
+            statistics.subcontractorCount,
+            statistics.adminCount,
+            statistics.hrCount,
+            statistics.employeeCount
+          ],
+          backgroundColor: "rgba(75, 192, 192, 0.6)",
+          borderColor: "rgba(75, 192, 192, 1)",
+          borderWidth: 1
+        }
+      ]
+    };
+
+    const chartOptions = {
+      scales: {
+        y: {
+          beginAtZero: true,
+          precision: 0
+        }
+      }
+    };
+
+    return <Bar data={chartData} options={chartOptions} />;
+  };
+
+  return (
+    <div className="p-4 border-2 border-gray-200 border rounded-lg dark:border-gray-700">
+
+      <h1 className="text-2xl font-bold mb-6">Tender Statistics</h1>
+      <div className="grid grid-cols-2 gap-4 mb-4">
+        <div className="flex items-center justify-center rounded bg-gray-50 h-68">
+          <div className="container">
+            {renderChart()}
+          </div>
+        </div>
+        <div className="flex items-center justify-center rounded bg-gray-50 h-68">
+          {statistics ? (
+            <>
+              <div className="grid grid-cols-2 gap-2">
+                <div className="bg-white rounded-lg shadow p-2 border border-gray-300">
+                  <h2 className="text-lg font-semibold mb-2">Total Tenders</h2>
+                  <p className="text-lg font-bold">{statistics.totalCount}</p>
+                </div>
+                <div className="bg-white rounded-lg shadow p-2 border border-gray-300">
+                  <h2 className="text-lg font-semibold mb-2">Reviewed and Approved Tenders</h2>
+                  <p className="text-lg font-bold">{statistics.activeApprovedCount}</p>
+                </div>
+                <div className="bg-white rounded-lg shadow p-2 border border-gray-300">
+                  <h2 className="text-lg font-semibold mb-2">Reviewed Tenders</h2>
+                  <p className="text-lg font-bold">{statistics.activeCount}</p>
+                </div>
+                <div className="bg-white rounded-lg shadow p-2 border border-gray-300">
+                  <h2 className="text-lg font-semibold mb-2">Approved Tenders</h2>
+                  <p className="text-lg font-bold">{statistics.approvedCount}</p>
+                </div>
+                <div className="bg-white rounded-lg shadow p-2 border border-gray-300">
+                  <h2 className="text-lg font-semibold mb-2">Contractor Tenders</h2>
+                  <p className="text-lg font-bold">{statistics.contractorCount}</p>
+                </div>
+                <div className="bg-white rounded-lg shadow p-2 border border-gray-300">
+                  <h2 className="text-lg font-semibold mb-2">Subcontractor Tenders</h2>
+                  <p className="text-lg font-bold">{statistics.subcontractorCount}</p>
+                </div>
+                <div className="bg-white rounded-lg shadow p-2 border border-gray-300">
+                  <h2 className="text-lg font-semibold mb-2">Admin Tenders</h2>
+                  <p className="text-lg font-bold">{statistics.adminCount}</p>
+                </div>
+                <div className="bg-white rounded-lg shadow p-2 border border-gray-300">
+                  <h2 className="text-lg font-semibold mb-2">HR Tenders</h2>
+                  <p className="text-lg font-bold">{statistics.hrCount}</p>
+                </div>
+                <div className="bg-white rounded-lg shadow p-2 border border-gray-300">
+                  <h2 className="text-lg font-semibold mb-2">Employee Tenders</h2>
+                  <p className="text-lg font-bold">{statistics.employeeCount}</p>
+                </div>
+              </div>
+            </>
+          ) : (
+
+            <p className="text-2xl font-bold">Loading</p>
+
+          )}
+        </div>
+
+      </div>
+    </div>
   );
 };
 
--- client/src/Admin/partials/dashboard/UserStatistics.jsx
+++ client/src/Admin/partials/dashboard/UserStatistics.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Bar } from 'react-chartjs-2';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 const UserStatistics = () => {
   const [statistics, setStatistics] = useState(null);
@@ -10,90 +13,114 @@ const UserStatistics = () => {
 
   const fetchUserStatistics = async () => {
     try {
-      const token = localStorage.getItem('token'); 
+      const token = localStorage.getItem('token');
       const config = {
         headers: {
           auth: token
         }
       };
 
-      const response = await axios.get('http://localhost:5000/apiTender/userdetails/statistics', config);
+      const response = await axios.get(
+        'http://localhost:5000/apiTender/userdetails/statistics',
+        config
+      );
       setStatistics(response.data);
     } catch (error) {
       console.log('Error fetching user statistics:', error);
     }
   };
 
+  const renderChart = () => {
+    if (!statistics) {
+      return null;
+    }
+
+    const chartData = {
+      labels: [
+
+        'Admin Users',
+        'Employee Users',
+        'HR Users',
+        'Regular Users',
+        'Subscription Active'
+      ],
+      datasets: [
+        {
+          label: 'User Statistics',
+          data: [
+            statistics.adminCount,
+            statistics.employeeCount,
+            statistics.hrCount,
+            statistics.userCount,
+            statistics.activeSubscriptionCount
+          ],
+          backgroundColor: 'rgba(75, 192, 192, 0.6)',
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 1
+        }
+      ]
+    };
+
+    const chartOptions = {
+      scales: {
+        y: {
+          beginAtZero: true,
+          precision: 0
+        }
+      }
+    };
+
+    return <Bar data={chartData} options={chartOptions} />;
+  };
+
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="p-4 border-2 border-gray-200 border rounded-lg dark:border-gray-700 mt-6">
+
       <h1 className="text-2xl font-bold mb-6">User Statistics</h1>
-      {statistics ? (
-        <div className="grid grid-cols-2 gap-4">
-        <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-          <h2 className="text-xl font-semibold mb-4">Total Users</h2>
-          <p className="text-2xl font-bold">{statistics.totalCount}</p>
+      <div className="grid grid-cols-2 gap-4 mb-4">
+        <div className="flex items-center justify-center rounded bg-gray-50 h-68">
+          <div className="container">
+            {renderChart()}
+          </div>
         </div>
-        <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-          <h2 className="text-xl font-semibold mb-4">Admin Users</h2>
-          <p className="text-2xl font-bold">{statistics.adminCount}</p>
-        </div>
-        <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-          <h2 className="text-xl font-semibold mb-4">Employee Users</h2>
-          <p className="text-2xl font-bold">{statistics.employeeCount}</p>
-        </div>
-        <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-          <h2 className="text-xl font-semibold mb-4">HR Users</h2>
-          <p className="text-2xl font-bold">{statistics.hrCount}</p>
-        </div>
-        <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-          <h2 className="text-xl font-semibold mb-4">Regular Users</h2>
-          <p className="text-2xl font-bold">{statistics.userCount}</p>
-        </div>
-        <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-          <h2 className="text-xl font-semibold mb-4">Subscription Active</h2>
-          <p className="text-2xl font-bold">{statistics.activeSubscriptionCount}</p>
-        </div>
-      </div>
+        <div className="flex items-center justify-center rounded bg-gray-50 h-68">
+        {statistics ? (
+            <>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="bg-white rounded-lg shadow p-2 border border-gray-300">
+              <h2 className="text-lg font-semibold mb-2">Total Users</h2>
+              <p className="text-lg font-bold">{statistics.totalCount}</p>
+            </div>
+            <div className="bg-white rounded-lg shadow p-2 border border-gray-300">
+              <h2 className="text-lg font-semibold mb-2">Admin Users</h2>
+              <p className="text-lg font-bold">{statistics.adminCount}</p>
+            </div>
+            <div className="bg-white rounded-lg shadow p-2 border border-gray-300">
+              <h2 className="text-lg font-semibold mb-2">Employee Users</h2>
+              <p className="text-lg font-bold">{statistics.employeeCount}</p>
+            </div>
+            <div className="bg-white rounded-lg shadow p-2 border border-gray-300">
+              <h2 className="text-lg font-semibold mb-2">HR Users</h2>
+              <p className="text-lg font-bold">{statistics.hrCount}</p>
+            </div>
+            <div className="bg-white rounded-lg shadow p-2 border border-gray-300">
+              <h2 className="text-lg font-semibold mb-2">Regular Users</h2>
+              <p className="text-lg font-bold">{statistics.userCount}</p>
+            </div>
+            <div className="bg-white rounded-lg shadow p-2 border border-gray-300">
+              <h2 className="text-lg font-semibold mb-2">Subscription Active</h2>
+              <p className="text-lg font-bold">{statistics.activeSubscriptionCount}</p>
+            </div>
+          </div>
+        </>
       ) : (
-        <div className="grid grid-cols-2 gap-4">
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4"></h2>
-            <p className="text-2xl font-bold"></p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4"></h2>
-            <p className="text-2xl font-bold"></p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4"></h2>
-            <p className="text-2xl font-bold"></p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4"></h2>
-            <p className="text-2xl font-bold"></p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4"></h2>
-            <p className="text-2xl font-bold"></p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4"></h2>
-            <p className="text-2xl font-bold"></p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4"></h2>
-            <p className="text-2xl font-bold"></p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4"></h2>
-            <p className="text-2xl font-bold"></p>
-          </div>
-          <div className="bg-white rounded-lg shadow p-6 border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4"></h2>
-            <p className="text-2xl font-bold"></p>
-          </div>
-        </div>
+        
+            <p className="text-2xl font-bold">Loading</p>
+          
       )}
+        </div>
+
+      </div>
     </div>
   );
 };
