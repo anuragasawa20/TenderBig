d9ca00172141dd940761799118fc98117987acb6
utkarsh somvanshi <somvanshi2132001@gmail.com>
2023-07-02 18:21:00 +0530

    commit-utkarshSomvanshi-#20

--- client/src/pages/sub-pages/JointVenture2.jsx
+++ client/src/pages/sub-pages/JointVenture2.jsx
@@ -2,8 +2,7 @@ import { useState } from "react";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import FileUpload from "../file-uploading/FileUpload";
-import JointVenture3 from "./JointVenture3";
-
+import { Link } from "react-router-dom";
 
 const JointVenture2 = () => {
     const [formData, setFormData] = useState({
@@ -135,128 +134,114 @@ const JointVenture2 = () => {
                 window.location.href = '/forms';
             });
     };
-
-    const [currentPage, setCurrentPage] = useState(2);
-
-    const nextPage = () => {
-        setCurrentPage(3);
-    };
-    const previousPage = () => {
-        setCurrentPage(1);
-    };
-
     return (
         <>
             <Navbar />
             <div className="max-w-3xl mx-auto mt-6 px-4 py-8 mb-6 shadow-2xl rounded-lg">
-                {currentPage === 2 && (
-                    <form onSubmit={handleSubmit}>
-                        {/* Global Section */}
-                        <h2 className="text-2xl font-bold mb-4">Personal details</h2>
-                        <label className="block mb-2 font-semibold relative">
-                            Full Name
-                            <span className="text-red-700 relative top-0 right-0">*</span>
-                            <input
-                                required
-                                type="text"
-                                name="name"
-                                value={formData.name}
-                                onChange={handleChange}
-                                className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
-                                placeholder="Name"
-                            />
-                        </label>
+                <form onSubmit={handleSubmit}>
+                    {/* Global Section */}
+                    <h2 className="text-2xl font-bold mb-4">Personal details</h2>
+                    <label className="block mb-2 font-semibold relative">
+                        Full Name
+                        <span className="text-red-700 relative top-0 right-0">*</span>
+                        <input
+                            required
+                            type="text"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
+                            placeholder="Name"
+                        />
+                    </label>
 
-                        <label className="block mb-2 font-semibold relative">
-                            Father Name
-                            <span className="text-red-700 relative top-0 right-0">*</span>
-                            <input
-                                required
-                                type="text"
-                                name="name"
-                                value={formData.name}
-                                onChange={handleChange}
-                                className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
-                            />
-                        </label>
+                    <label className="block mb-2 font-semibold relative">
+                        Father Name
+                        <span className="text-red-700 relative top-0 right-0">*</span>
+                        <input
+                            required
+                            type="text"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
+                        />
+                    </label>
 
-                        <label className="block mb-2 font-semibold relative">
-                            Director / Company Post
-                            <span className="text-red-700 relative top-0 right-0">*</span>
-                            <input
-                                required
-                                type="text"
-                                name="name"
-                                value={formData.name}
-                                onChange={handleChange}
-                                className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
-                            />
-                        </label>
+                    <label className="block mb-2 font-semibold relative">
+                        Director / Company Post
+                        <span className="text-red-700 relative top-0 right-0">*</span>
+                        <input
+                            required
+                            type="text"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
+                        />
+                    </label>
 
-                        <label className="block mb-2 font-semibold relative">
-                            Email
-                            <span className="text-red-700 relative top-0 right-0">*</span>
-                            <input
-                                required
-                                type="email"
-                                name="email"
-                                value={formData.email}
-                                onChange={handleChange}
-                                className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
-                            />
-                        </label>
+                    <label className="block mb-2 font-semibold relative">
+                        Email
+                        <span className="text-red-700 relative top-0 right-0">*</span>
+                        <input
+                            required
+                            type="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
+                        />
+                    </label>
 
-                        <label className="block mb-2 font-semibold relative">
-                            Contact Number
-                            <span className="text-red-700 relative top-0 right-0">*</span>
-                            <input
-                                required
-                                type="number"
-                                name="pnumber"
-                                value={formData.pnumber}
-                                onChange={handleChange}
-                                className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
-                            />
-                        </label>
+                    <label className="block mb-2 font-semibold relative">
+                        Contact Number
+                        <span className="text-red-700 relative top-0 right-0">*</span>
+                        <input
+                            required
+                            type="number"
+                            name="pnumber"
+                            value={formData.pnumber}
+                            onChange={handleChange}
+                            className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
+                        />
+                    </label>
 
-                        <label className="block mb-2 font-semibold relative">
-                            16-Digit Aadhar Number
-                            <span className="text-red-700 relative top-0 right-0">*</span>
-                            <input
-                                required
-                                type="text"
-                                name="aadhar"
-                                value={formData.aadhar}
-                                onChange={handleChange}
-                                className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
-                                placeholder="Aadhar Number"
-                            />
-                        </label>
+                    <label className="block mb-2 font-semibold relative">
+                        16-Digit Aadhar Number
+                        <span className="text-red-700 relative top-0 right-0">*</span>
+                        <input
+                            required
+                            type="text"
+                            name="aadhar"
+                            value={formData.aadhar}
+                            onChange={handleChange}
+                            className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
+                            placeholder="Aadhar Number"
+                        />
+                    </label>
 
-                        <div>
-                            <p className="font-bold">Upload you passport size photo</p>
-                        </div>
-                        <FileUpload />
+                    <div>
+                        <p className="font-bold">Upload you passport size photo</p>
+                    </div>
+                    <FileUpload />
+                    <Link to="/jointventure1">
                         <button
                             type="button"
-                            onClick={nextPage}
+                            className="bg-red-700 text-white px-4 py-2 mt-8 rounded hover:bg-red-800 mx-1"
+                        >
+                            Previous
+                        </button>
+                    </Link>
+                    <Link to='/JointVenture3'>
+                        <button
+                            type="button"
                             className="bg-red-700 text-white px-4 py-2 mt-8 rounded hover:bg-red-800"
                         >
                             Next
                         </button>
-                    </form>
-                )}
-
-                {currentPage === 3 && (
-                    <form onSubmit={handleSubmit}>
-                        <JointVenture3
-                            formData={formData}
-                            handleChange={handleChange}
-                            handleSubmit={handleSubmit}
-                            previousPage={previousPage}
-                        />
-                    </form>
-                )}
+                    </Link>
+                </form>
             </div>
             <Footer />
         </>
@@ -264,38 +249,3 @@ const JointVenture2 = () => {
 };
 
 export default JointVenture2;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- client/src/pages/sub-pages/JointVenture3.jsx
+++ client/src/pages/sub-pages/JointVenture3.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
+import { Link } from "react-router-dom";
 
 
 const JointVenture3 = () => {
@@ -134,142 +135,124 @@ const JointVenture3 = () => {
             });
     };
 
-    const [currentPage, setCurrentPage] = useState(3);
-
-    const previousPage = () => {
-        setCurrentPage(2);
-    };
-
     return (
         <>
             <Navbar />
             <div className="max-w-3xl mx-auto mt-6 px-4 py-8 mb-6 shadow-2xl rounded-lg">
-                {currentPage === 3 && (
-                    <form onSubmit={handleSubmit}>
-                        {/* Global Section */}
-                        <h2 className="text-2xl font-bold mb-4">Partnership</h2>
-                        <label className="block mb-2 font-semibold relative">
-                            Company profile
+                <form onSubmit={handleSubmit}>
+                    {/* Global Section */}
+                    <h2 className="text-2xl font-bold mb-4">Partnership</h2>
+                    <label className="block mb-2 font-semibold relative">
+                        Company profile
+                        <span className="text-red-700 relative top-0 right-0">*</span>
+                        <input
+                            required
+                            type="text"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
+                        />
+                    </label>
+
+                    <label className="block mb-2 font-semibold relative">
+                        Partnership project / tender
+                        <span className="text-red-700 relative top-0 right-0">*</span>
+                        <input
+                            required
+                            type="text"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
+                        />
+                    </label>
+
+                    <label className="block mb-2 font-semibold relative">
+                        Requirement : Finance / manpower (select one)
+                        <span className="text-red-700 relative top-0 right-0">*</span>
+                        <input
+                            required
+                            type="text"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
+                            placeholder="Name"
+                        />
+                    </label>
+
+                    <div className="grid grid-cols-2 gap-4 mt-1.5 mb-1.5">
+                        <label className="block font-semibold">
+                            Start Date
+                            <span className="text-red-700 relative top-0 right-0">*</span>
+                            <input
+                                type="date"
+                                name="startDate"
+                                value={formData.startDate}
+                                onChange={handleChange}
+                                className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
+                                placeholder="Enter Start Date"
+                            />
+                        </label>
+                        <label className="block font-semibold">
+                            End Date
                             <span className="text-red-700 relative top-0 right-0">*</span>
                             <input
                                 required
-                                type="text"
-                                name="name"
-                                value={formData.name}
+                                type="date"
+                                name="endDate"
+                                value={formData.endDate}
                                 onChange={handleChange}
                                 className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
+                                placeholder="Enter End Date"
                             />
                         </label>
+                    </div>
 
-                        <label className="block mb-2 font-semibold relative">
-                            Partnership project / tender
-                            <span className="text-red-700 relative top-0 right-0">*</span>
-                            <input
-                                required
-                                type="text"
-                                name="name"
-                                value={formData.name}
-                                onChange={handleChange}
-                                className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
-                            />
-                        </label>
+                    <label className="block mb-2 font-semibold">
+                        Volume
+                        <span className="text-red-700 relative top-0 right-0">*</span>
+                        <input
+                            required
+                            type="number"
+                            name="TotalValuation"
+                            value={formData.TotalValuation}
+                            onChange={handleChange}
+                            className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
 
-                        <label className="block mb-2 font-semibold relative">
-                            Requirement : Finance / manpower (select one)
-                            <span className="text-red-700 relative top-0 right-0">*</span>
-                            <input
-                                required
-                                type="text"
-                                name="name"
-                                value={formData.name}
-                                onChange={handleChange}
-                                className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
-                                placeholder="Name"
-                            />
-                        </label>
+                        />
+                    </label>
 
-                        <div className="grid grid-cols-2 gap-4 mt-1.5 mb-1.5">
-                            <label className="block font-semibold">
-                                Start Date
-                                <span className="text-red-700 relative top-0 right-0">*</span>
-                                <input
-                                    type="date"
-                                    name="startDate"
-                                    value={formData.startDate}
-                                    onChange={handleChange}
-                                    className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
-                                    placeholder="Enter Start Date"
-                                />
-                            </label>
-                            <label className="block font-semibold">
-                                End Date
-                                <span className="text-red-700 relative top-0 right-0">*</span>
-                                <input
-                                    required
-                                    type="date"
-                                    name="endDate"
-                                    value={formData.endDate}
-                                    onChange={handleChange}
-                                    className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
-                                    placeholder="Enter End Date"
-                                />
-                            </label>
-                        </div>
-
-                        <label className="block mb-2 font-semibold">
-                            Volume
-                            <span className="text-red-700 relative top-0 right-0">*</span>
-                            <input
-                                required
-                                type="number"
-                                name="TotalValuation"
-                                value={formData.TotalValuation}
-                                onChange={handleChange}
-                                className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
-
-                            />
-                        </label>
-
-                        <label className="block mb-2 font-semibold relative">
-                            Work experience (if man-power required)
-                            <span className="text-red-700 relative top-0 right-0">*</span>
-                            <input
-                                required
-                                type="email"
-                                name="email"
-                                value={formData.email}
-                                onChange={handleChange}
-                                className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
-                                placeholder="Email"
-                            />
-                        </label>
+                    <label className="block mb-2 font-semibold relative">
+                        Work experience (if man-power required)
+                        <span className="text-red-700 relative top-0 right-0">*</span>
+                        <input
+                            required
+                            type="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            className="border rounded-sm px-3 py-2 mt-1 w-full text-black bg-gray-100 focus:border-red-700 focus:ring-2 focus:ring-red-700 focus:outline-none"
+                            placeholder="Email"
+                        />
+                    </label>
+                    <Link to='/JointVenture2'>
                         <button
                             type="button"
-                            onClick={previousPage}
                             className="bg-red-700 text-white px-4 py-2 mt-8 rounded hover:bg-red-800"
                         >
                             Back
                         </button>
-                        <button
-                            type="button"
-                            onClick={handleSubmit}
-                            className="bg-red-700 mx-2 text-white px-4 py-2 mt-8 rounded hover:bg-red-800"
-                        >
-                            Submit
-                        </button>
-                    </form>
-                )}
-
-                {currentPage === 2 && (
-                    <form onSubmit={handleSubmit}>
-                        <JointVenture3
-                            formData={formData}
-                            handleChange={handleChange}
-                            handleSubmit={handleSubmit}
-                            previousPage={previousPage}
-                        />
-                    </form>
-                )}
+                    </Link>
+                    <button
+                        type="button"
+                        onClick={handleSubmit}
+                        className="bg-red-700 mx-2 text-white px-4 py-2 mt-8 rounded hover:bg-red-800"
+                    >
+                        Submit
+                    </button>
+                </form>
             </div>
             <Footer />
         </>
