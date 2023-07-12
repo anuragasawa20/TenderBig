086c1aa0e4f32e942b917f666a3127232e1f38e7
anuragasawa20 <img_2021012@iiitm.ac.in>
2023-07-12 12:10:27 +0530

    adding data in userDashboard

--- client/src/Admin/partials/dashboard/userDashboards/UserDetails.jsx
+++ client/src/Admin/partials/dashboard/userDashboards/UserDetails.jsx
@@ -3,9 +3,12 @@ import UserCards from './UserCards';
 
 
 const UserDetails = () => {
-    const [forms, setFormData] = useState(null);
+    const [forms, setFormData] = useState([]);
     const [userData, setUserData] = useState([]);
-
+    let forms1 = [];
+    let userData1 = localStorage.getItem("user");
+    let userDataObject = JSON.parse(userData1);
+    let id1 = userDataObject.userId;
     useEffect(() => {
         // Fetch data from the API
         let userData1 = localStorage.getItem("user");
@@ -19,33 +22,35 @@ const UserDetails = () => {
             .then((response) => response.json())
             .then((data) => {
                 console.log(data);
-                setUserData(data)
+                // forms1.push(data)
+                setFormData(data);
+                console.log(forms);
             }
             )
             .catch((error) => console.log(error));
-    }, [])
+    }, [id1, forms])
 
-
+    let careerCount = forms[2].number + forms[1].number
 
     return (
         <div className="flex justify-center m-5">
             <div className="card-container mx-5">
-                <UserCards className="my-5" description="746834" title="Career & Manpower" />
-                <UserCards className="my-5" description="322" title="Registrations" />
-                <UserCards className="my-5" description="345345" title="Company Certifications" />
+                <UserCards className="my-5" description={`Counts : ${careerCount}`} title="Career & Manpower" />
+                <UserCards className="my-5" description={`Counts : ${forms[6].number}`} title="Registrations" />
+                <UserCards className="my-5" description={`Counts : ${forms[5].number}`} title="Company Certifications" />
             </div>
             <div className="card-container mx-5">
-                <UserCards className="my-5" description="Numbers and Counts" title="Licenses" />
-                <UserCards className="my-5" description="345345" title="Indivadual Certifications" />
-                <UserCards className="my-5" description="Numbers and Counts" title="Auction Materials" />
+                <UserCards className="my-5" description={`Counts : ${forms[1].number}`} title="Licenses" />
+                <UserCards className="my-5" description={`Counts : ${forms[4].number}`} title="Individual Certifications" />
+                <UserCards className="my-5" description={`Counts : ${forms[0].number}`} title="Auction Materials" />
             </div>
             <div className="card-container mx-5">
-                <UserCards className="my-5" description="Numbers and Counts" title="Offline Tender Filling" />
-                <UserCards className="my-5" description="Numbers and Counts" title="Gem Registration" />
+                <UserCards className="my-5" description={`Counts : ${forms[8].number}`} title="Offline Tender Filling" />
+                <UserCards className="my-5" description={`Counts : ${forms[10].number}`} title="Gem Registration" />
             </div>
             <div className='card-container mx-5'>
-                <UserCards className="my-5" description="Numbers and Counts" title="Joint Venture" />
-                <UserCards className="my-5" description="Numbers and Counts" title="Online Tender Filling" />
+                <UserCards className="my-5" description={`Counts : ${forms[7].number}`} title="Joint Venture" />
+                <UserCards className="my-5" description={`Counts : ${forms[9].number}`} title="Online Tender Filling" />
             </div>
         </div>
     );
